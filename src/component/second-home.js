048051a4b990d1deadb80d1ceeeb5033674d8a28
ShreePrasad Das <shreepsd2@gmail.com>
2025-06-26 14:40:13 +0545

    changes in home-container img tag

--- src/component/second-home.js
+++ src/component/second-home.js
@@ -21,7 +21,8 @@ export default function SecondHome() {
     
   }
   const profileImage= <div className="main-home-image-section">
-    <img src={ProfileIMG} alt="profile image" />
+   <img src={ProfileIMG} alt="Shree Prasad's profile" />
+
     <svg className="home-svg1" id="eQMZKBaLNFf1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 300 300" shapeRendering="geometricPrecision" textRendering="geometricPrecision" project-id="570795cb7d1a43608ca02fb39cb12a43" export-id="11011a9321584b159543a503af35a6c8" cached="false"><defs><linearGradient id="eQMZKBaLNFf2-fill" x1="0.136807" y1={1} x2="0.879059" y2="0.018992" spreadMethod="pad" gradientUnits="objectBoundingBox" gradientTransform="translate(0 0)"><stop id="eQMZKBaLNFf2-fill-0" offset="0%" stopColor="rgba(19,160,160,0.39)" /><stop id="eQMZKBaLNFf2-fill-1" offset="100%" stopColor="rgba(245,255,255,0.26)" /></linearGradient></defs><rect width="243.529412" height="214.588235" rx={44} ry={44} transform="matrix(.889853 0 0 1.046052 43.412017 26.823598)" fill="url(#eQMZKBaLNFf2-fill)" strokeWidth={0} /></svg>
 
   </div>
